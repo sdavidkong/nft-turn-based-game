684dad5b68245c018d59c88e8dc555af43205734
S David Kong <76503030+sdavidkong@users.noreply.github.com>
2022-04-14 16:07:08 +0300

    Add files via upload

---
+++ scripts/deploy.js
@@ -0,0 +1,30 @@
+const main = async () => {
+  const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
+  const gameContract = await gameContractFactory.deploy(
+    ["Not Leo", "Not Aang", "Not Pikachu"],       // Names
+    ["https://i.imgur.com/pKd5Sdk.png", // Images
+    "https://i.imgur.com/xVu4vFL.png", 
+    "https://i.imgur.com/WMB6g9u.png"],
+    [100, 200, 300],                    // HP values
+    [100, 50, 25],
+    "Not Elon Musk", // Boss name
+    "https://i.imgur.com/AksR0tt.png", // Boss image
+    1000, // Boss hp
+    50 // Boss attack damage     
+  );
+  await gameContract.deployed();
+  console.log("Contract deployed to:", gameContract.address);
+
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
---
+++ scripts/run.js
@@ -0,0 +1,47 @@
+const main = async () => {
+    const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
+    const gameContract = await gameContractFactory.deploy(
+      ["Not Leo", "Not Aang", "Not Pikachu"],       // Names
+      ["https://i.imgur.com/pKd5Sdk.png", // Images
+      "https://i.imgur.com/xVu4vFL.png", 
+      "https://i.imgur.com/WMB6g9u.png"],
+      [100, 200, 300],                    // HP values
+      [100, 50, 25],
+      "Not Elon Musk", // Boss name
+      "https://i.imgur.com/AksR0tt.png", // Boss image
+      1000, // Boss hp
+      50 // Boss attack damage     
+    );
+    await gameContract.deployed();
+    console.log("Contract deployed to:", gameContract.address);
+    let txn;
+    txn = await gameContract.mintCharacterNFT(2);
+    await txn.wait();
+
+    let returnedTokenUri = await gameContract.tokenURI(1);
+    console.log("Token URI:", returnedTokenUri);
+
+    txn = await gameContract.mintCharacterNFT(2);
+    await txn.wait();
+
+    txn = await gameContract.attackBoss();
+    await txn.wait();
+
+    txn = await gameContract.attackBoss();
+    await txn.wait();
+
+    console.log("Done!");
+
+  };
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
