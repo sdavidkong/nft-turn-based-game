2b11592caaf4e22b1f592ce0c191a99e8a162c8c
S David Kong <76503030+sdavidkong@users.noreply.github.com>
2022-04-14 16:08:54 +0300

    Create constants.jsx

---
+++ src/constants.jsx
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0x074Fdb8A9B5eb121573810D8779bdae9f6795a81';
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
